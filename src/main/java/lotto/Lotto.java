43e2853e501ef9b189cfb171a77a8025fddec191
leeminho <tommymh97@naver.com>
2025-11-03 23:18:48 +0900

    feat(lotto): 로또 번호 유효성 검증 및 정렬 기능 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,20 +1,39 @@
 package lotto;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
 
 public class Lotto {
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
         validate(numbers);
-        this.numbers = numbers;
+        this.numbers = new ArrayList<>(numbers);
+        Collections.sort(this.numbers);
     }
 
     private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+        if (numbers.size() != Constants.LOTTO_SIZE) {
+            throw new IllegalArgumentException(Message.ERROR_INVALID_SIZE);
+        }
+        Set<Integer> unique = new HashSet<>(numbers);
+        if (unique.size() != Constants.LOTTO_SIZE) {
+            throw new IllegalArgumentException(Message.ERROR_DUPLICATE_LOTTO);
+        }
+        for (int number : numbers) {
+            new LottoNumber(number);
         }
     }
 
-    // TODO: 추가 기능 구현
+    public List<Integer> getNumbers() {
+        return Collections.unmodifiableList(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
 }
